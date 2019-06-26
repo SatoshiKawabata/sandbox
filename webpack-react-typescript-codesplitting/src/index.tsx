1126d4bd2b8f7845790c5e2b9ae867c79db02a9a
Satoshi Kawabata <satoshi.kawabata@nulab-inc.com>
2019-06-26 16:51:57 +0900

    update

--- webpack-react-typescript-codesplitting/src/index.tsx
+++ webpack-react-typescript-codesplitting/src/index.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import loadable from '@loadable/component';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import loadable from "@loadable/component";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
-const Home = loadable(() => import('./pages/Home'));
-const Page1 = loadable(() => import('./pages/Page1'));
-const Page2 = loadable(() => import('./pages/Page2'));
+const Home = loadable(() => import("./pages/Home"));
+const Page1 = loadable(() => import("./pages/Page1"));
+const Page2 = loadable(() => import("./pages/Page2"));
 
 const { Suspense } = React;
 
 ReactDOM.render(
-  [
-    <h1>hello meerkat🐈</h1>,
+  <>
+    <h1>hello</h1>
     <Router>
       <nav>
         <ul>
@@ -28,12 +28,12 @@ ReactDOM.render(
       </nav>
       <Suspense fallback={<div>Loading...</div>}>
         <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route path="/page1" component={Page1}/>
-          <Route path="/page2" component={Page2}/>
+          <Route exact path="/" component={Home} />
+          <Route path="/page1" component={Page1} />
+          <Route path="/page2" component={Page2} />
         </Switch>
       </Suspense>
     </Router>
-  ],
+  </>,
   document.querySelector("#app")
 );

aeb25c8e3b87e2ff1c048a748ac296339cdd0a6e
Satoshi Kawabata <kwst.andromeda.ma@gmail.com>
2020-07-19 00:46:20 +0900

    imple chat sample

--- socketio-practice/src/main.ts
+++ socketio-practice/src/main.ts
@@ -1,12 +1,30 @@
 import express from "express";
+import http from "http";
+import socketIo from "socket.io";
 const app = express();
+
+const httpServer = http.createServer(app);
+const io = socketIo(httpServer);
 
 const PORT = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    socket.broadcast.emit("chat message", msg);
+    // io.emit("chat message", msg);
+  });
 });
